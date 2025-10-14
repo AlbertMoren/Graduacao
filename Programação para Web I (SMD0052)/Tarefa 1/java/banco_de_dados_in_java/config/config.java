57ed78243224552ae3885de09d139236158ec3f7
Albert <albertmorenn@gmail.com>
2025-10-14 15:30:09 -0300

    update

---
+++ Programação para Web I (SMD0052)/Tarefa 1/java/banco_de_dados_in_java/config/config.java
@@ -0,0 +1,12 @@
+package banco_de_dados_in_java.config;
+
+public final class config {
+    public static final String BD_DRIVER = "org.postgresql.Driver";
+    public static final String BD_URL = "jdbc:postgresql://localhost:5432/meubanco";
+    public static final String BD_USUARIO = "postgres";
+    public static final String BD_SENHA = "minhasenhafacil123";
+
+    private config(){
+
+    }
+}
